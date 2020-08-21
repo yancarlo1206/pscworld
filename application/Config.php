70d136099e20b517a91bd951aff286b5a6c1ce88
Yan <yancarlo1206@gmail.com>
2020-08-20 22:26:02 -0500

    Primer Commit

---
+++ application/Config.php
@@ -0,0 +1,33 @@
+<?php
+
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_LAYOUT', 'default');
+define('APP_NAME', 'Sistema PSCWORLD');
+define('APP_EMAIL', 'contacto@pscworld.co');
+define('APP_SLOGAN', 'PSCWORLD');
+define('APP_COMPANY', 'www.pscworld.co');
+define('SESSION_TIME', 100);
+define('HASH_KEY', '4eba6d532be78');
+
+/*if (in_array(@$_SERVER['REMOTE_ADDR'], array(
+    '127.0.0.1',
+    '::1'
+))) {
+	define('BASE_URL', 'http://localhost/mvc_base_2018/');
+	define('DB_HOST', 'localhost');
+	define('DB_HOST_LOCAL', true);	
+}else {
+	
+	define('BASE_URL', 'http://tksis.com/');
+	define('DB_HOST', 'localhost');	
+	define('DB_HOST_LOCAL', false);	
+}*/
+define('BASE_URL', 'http://localhost/pscworld/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pscworld');
+define('DB_PORT', '');
+define('DB_CHAR', 'utf8');
+
+?>
---
+++ views/layout/default/footer.php
@@ -0,0 +1,132 @@
+<footer class="main-footer">
+        <div class="auto-container">
+            
+            <div class="widgets-section">
+                <div class="row">
+                    <div class="big-column col-xl-8 col-lg-12 col-md-12">
+                        <div class="row">
+                            <!--Footer Column-->
+                            <div class="footer-column col-lg-3 col-md-12 col-sm-12">
+                                <div class="logo"><a href="#"><img src="images/logo.png" alt=""></a></div>
+                            </div>
+
+                            <div class="footer-column col-lg-3 col-md-4 col-sm-12">
+                                <div class="footer-widget links-widget">
+                                    <h2 class="widget-title">Certificación</h2>
+                                    <div class="widget-content">
+                                        <ul class="list">
+                                            <li><a href="certificadosSSL.html">Certificado SSL</a></li>
+                                            <li><a href="nom151.html">Constancia NOM-151</a></li>
+                                            <li><a href="doc2sign.html">Doc2sign</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="footer-column col-lg-4 col-md-4 col-sm-12">
+                                <div class="footer-widget links-widget">
+                                    <h2 class="widget-title">Conócenos</h2>
+                                    <div class="widget-content">
+                                        <ul class="list">
+                                            <li><a href="servicios.html">Servicios</a></li>
+                                            <li><a href="noticias.html">Noticias</a></li>
+                                            <li><a href="contact.html">Contacto</a></li>
+                                            <li><a href="terminosCondiciones.html">Terminos y condiciones</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- <div class="footer-column col-lg-3 col-md-4 col-sm-12">
+                                <div class="footer-widget links-widget">
+                                    <h2 class="widget-title">Page Links</h2>
+                                    <div class="widget-content">
+                                        <ul class="list">
+                                            <li><a href="#">Cloud Services</a></li>
+                                            <li><a href="#">Shop</a></li>
+                                            <li><a href="#">Contacto</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div> -->
+                        </div>
+                    </div>
+
+                    <div class="big-column col-xl-4 col-lg-12 col-md-12">
+                        <div class="row">
+                            <div class="footer-column col-lg-12 col-md-12 col-sm-12">
+                                <div class="footer-widget links-widget">
+                                    <h2 class="widget-title">Subscribete</h2>
+                                    <div class="widget-content">
+                                        <div class="newsletter-form">
+                                            <form method="post" action="#" id="subscribe-form">
+                                                <div class="form-group"><div class="response"></div></div>
+                                                <div class="form-group">
+                                                    <input type="email" name="email" class="email" value="" placeholder="Ingrese su correo electrónico.." required>
+                                                    <button type="button" id="subscribe-newslatters" class="theme-btn"><i class="flaticon-arrow-pointing-to-right"></i></button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Call Us -->
+            <div class="call-us">
+                <div class="service-num"><a href="#"><i class="fa fa-phone"></i>+52 55 5339 6600</a></div>
+                <div class="social-link">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fab fa-vimeo-v"></i></a>
+                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                </div>
+            </div>
+        </div>
+        
+
+        <!--Bottom-->
+        <div class="footer-bottom">
+            <div class="auto-container">
+                <div class="copyright-text">
+                    © Copyright 2004-2020 PSC World SA de CV, Todos los derechos reservados.    
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+    <!-- End Main Footer -->
+
+</div><!-- End Page Wrapper -->
+
+<!-- Scroll To Top -->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="flaticon-arrow-up"></span></div>
+
+<script src="<?php echo BASE_URL; ?>public/js/jquery.js"></script> 
+<script src="<?php echo BASE_URL; ?>public/js/popper.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/js/bootstrap.min.js"></script>
+<!--Revolution Slider-->
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="<?php echo BASE_URL; ?>public/js/main-slider-script.js"></script>
+<!--Revolution Slider-->
+<script src="<?php echo BASE_URL; ?>public/js/jquery.fancybox.js"></script>
+<script src="<?php echo BASE_URL; ?>public/js/owl.js"></script>
+<script src="<?php echo BASE_URL; ?>public/js/wow.js"></script>
+<script src="<?php echo BASE_URL; ?>public/js/appear.js"></script>
+<script src="<?php echo BASE_URL; ?>public/js/script.js"></script>
+<script src="<?php echo BASE_URL; ?>public/js/modalimg.js"></script>
+</body>
+</html>
