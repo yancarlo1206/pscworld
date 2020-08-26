c015bc8803fdfd21225c10f3cdcc06c9ed04e121
brayanranda <brayanranda@gmail.com>
2020-08-26 11:06:57 -0500

    Agregar boton editar

--- application/Controller.php
+++ application/Controller.php
@@ -209,7 +209,7 @@ abstract class Controller {
     if(isset($_POST[$clave]) && !empty($_POST[$clave])){
       $_POST[$clave] = strip_tags($_POST[$clave]);
       if(!get_magic_quotes_gpc()){
-        $_POST[$clave] = @mysql_escape_string($_POST[$clave]);
+        // $_POST[$clave] = @mysql_escape_string($_POST[$clave]);
       }
       return trim($_POST[$clave]);
     }
