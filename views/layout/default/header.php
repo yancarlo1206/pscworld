643551347a61ab8af827d2343f846445e7e76af5
brayanranda <brayanranda@gmail.com>
2020-08-26 22:12:56 -0500

    Nueva tabla lista

--- views/layout/default/header.php
+++ views/layout/default/header.php
@@ -125,9 +125,21 @@
                     <div class="nav-logo"><a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/images/logo-5.png" alt="" title="" ></a></div><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
                 </nav>
 
-                <div class="menu-search">
-                    <a href="<?php echo BASE_URL; ?>login">Iniciar Sesion</a>
-                </div>
+                <?php if(!Session::get('autenticado')){ ?>
+                    <div class="menu-search">
+                        <a href="<?php echo BASE_URL; ?>login">
+                            <i class="fa flaticon-user"></i>
+                            Iniciar sesión
+                        </a>
+                        </div>
+                <?php }else{ ?>
+                    <div class="menu-search">
+                        <a href="<?php echo BASE_URL; ?>login/cerrar">
+                            <i class="fa flaticon-arrow-pointing-to-right"></i>
+                            Cerrar sesión
+                        </a>
+                        </div>
+                <?php } ?>
             </div>
         </div><!-- End Mobile Menu -->
     </header>
