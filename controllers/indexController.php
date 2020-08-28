8378404a250df184a176a200ed213f61547645ba
Yan <yancarlo1206@gmail.com>
2020-08-27 22:46:56 -0500

    Ultimas noticias dinamicas

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -4,11 +4,13 @@ class indexController extends Controller {
     public function __construct() {
         parent::__construct();
         $this->_banner = $this->loadModel('banners');
+        $this->_noticia = $this->loadModel('noticias');
         $this->_view->setJs(array('modalimg'));
     }
     
     public function index() {
     	$this->_view->banners = $this->_banner->resultList();
+        $this->_view->noticias = $this->_noticia->resultList();
     	$this->_view->titulo = 'Inicio PscWorld';
         $this->_view->renderizar('index', 'inicio');
     }
