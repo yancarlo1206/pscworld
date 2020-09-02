7d821c9197881ffda8be4389b7c413ffe613b2fb
Yan <yancarlo1206@gmail.com>
2020-09-02 11:39:15 -0500

    Limite de noticias en el index...

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -10,7 +10,8 @@ class indexController extends Controller {
     
     public function index() {
     	$this->_view->banners = $this->_banner->resultList();
-        $this->_view->noticias = $this->_noticia->resultList();
+        //$this->_view->noticias = $this->_noticia->resultList();
+        $this->_view->noticias = $this->_noticia->dql("select n from Entities\Noticias n order by n.id desc",array());
     	$this->_view->titulo = 'Inicio PscWorld';
         $this->_view->renderizar('index', 'inicio');
     }
